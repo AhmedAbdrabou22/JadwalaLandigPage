0301c8c028aa0b4c9a94a680a763e9ddc8e7a4c1
Ahmed <ahmedabdrabou723@gmail.com>
2022-08-23 16:45:57 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// let listGroubItems = document.querySelectorAll(".list-group li");
+// let filterItems = document.querySelectorAll(".filter-item a");
+
+// listGroubItems.forEach(list=>{
+//     list.addEventListener("click",()=>{
+//         document.querySelector(".list-group li.active").classList.remove("active")
+//         list.classList.add("active")
+//         let dataFilter = list.dataset.filter;
+//         filterItems.forEach(item=>{
+//             if(item.classList.contains(dataFilter)){
+//                 item.classList.remove("hidden");
+//                 item.classList.add("active");
+//             }else{
+//                 item.classList.remove("active");
+//                 item.classList.add("hidden");
+//             }
+//         })
+//     })
+// })
+
+// // Gallery in protflio
+// lightGallery(document.getElementById('ligtGallery'), {
+// });
