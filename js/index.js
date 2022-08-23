50c03a90cc2c2cdf87e027fe9616c37c109b0e84
Ahmed <ahmedabdrabou723@gmail.com>
2022-08-23 19:38:14 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,23 +1,11 @@
-// let listGroubItems = document.querySelectorAll(".list-group li");
-// let filterItems = document.querySelectorAll(".filter-item a");
+let date = new Date();
+let copy = document.querySelector('.copy')
+copy.innerHTML = `Copyright&copy;Jadwal ${date.getFullYear()}`
 
-// listGroubItems.forEach(list=>{
-//     list.addEventListener("click",()=>{
-//         document.querySelector(".list-group li.active").classList.remove("active")
-//         list.classList.add("active")
-//         let dataFilter = list.dataset.filter;
-//         filterItems.forEach(item=>{
-//             if(item.classList.contains(dataFilter)){
-//                 item.classList.remove("hidden");
-//                 item.classList.add("active");
-//             }else{
-//                 item.classList.remove("active");
-//                 item.classList.add("hidden");
-//             }
-//         })
-//     })
-// })
-
-// // Gallery in protflio
-// lightGallery(document.getElementById('ligtGallery'), {
-// });
+let main = document.querySelector(".main")
+let scrollbtn = document.querySelector(".scrollbtn")
+scrollbtn.addEventListener("click",()=>{
+    main.scrollIntoView({
+        behavior: "smooth"
+    })
+})
